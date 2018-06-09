fe6e24450cf4bdea66099c682cdbeca684c84866
mpochron <7736131+mpochron@users.noreply.github.com>
2018-06-09 17:33:36 +0200

    Update Backup.py

--- backup/src/fr/webcenter/backup/Backup.py
+++ backup/src/fr/webcenter/backup/Backup.py
@@ -321,7 +321,7 @@ class Backup(object):
             logger.debug("Directory '%s' already exist", target_dir)
 
         commandService.runCmd("docker pull %s" % image)
-        command = "sh -c 'mysqldump -h %s --column-statistics=0 -P %s -u %s %s > %s/%s.dump'" % (listDatabaseSettings['host'], listDatabaseSettings['port'], listDatabaseSettings['user'], listDatabaseSettings['name'], target_dir, listDatabaseSettings['name'])
+        command = "sh -c 'mysqldump -h %s --column-statistics=0 -P %s -u %s %s' > %s/%s.dump" % (listDatabaseSettings['host'], listDatabaseSettings['port'], listDatabaseSettings['user'], listDatabaseSettings['name'], target_dir, listDatabaseSettings['name'])
         dockerCmd = "docker run --rm --privileged -v %s:%s -e 'MYSQL_PWD=%s' %s %s" % (target_dir, target_dir, listDatabaseSettings['password'], image, command)
         commandService.runCmd(dockerCmd)
         logger.info("Dump Rancher database is finished")
